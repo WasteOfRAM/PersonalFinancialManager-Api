c52c415994e620a7a8c682031eb5239761913562
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-06-19 14:52:12 +0300

    Added infrastucture with ef dbcontext.

---
+++ src/PersonalFinancialManager.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace PersonalFinancialManager.Infrastructure.Data;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PersonalFinancialManager.Data.Entities;
+
+public class AppDbContext(DbContextOptions options) : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
