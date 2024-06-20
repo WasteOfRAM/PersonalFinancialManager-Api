4c25b162b3a83b4db6b894d12977d293bbec6f06
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-06-20 14:47:23 +0300

    Adding DbContext, Identity and Jwt to service collection.

---
+++ src/PersonalFinancialManager.Infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,63 @@
+﻿namespace PersonalFinancialManager.Infrastructure;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using PersonalFinancialManager.Data.Entities;
+using PersonalFinancialManager.Infrastructure.Data;
+using System.Text;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddAppDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DbConnection"));
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddIdentityCore<AppUser>(options =>
+        {
+            options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._";
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireDigit = true;
+            options.Password.RequiredLength = 8;
+        })
+            .AddRoles<IdentityRole<Guid>>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+        return services;
+    }
+
+    public static IServiceCollection AddTokenService(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!)),
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    ValidateIssuer = true,
+                    ValidateAudience = true
+                };
+            });
+
+        return services;
+    }
+}
--- src/PersonalFinancialManager.WebApi/Program.cs
+++ src/PersonalFinancialManager.WebApi/Program.cs
@@ -1,11 +1,22 @@
+using PersonalFinancialManager.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAppDbContext(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddTokenService(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
