a5283bc834a21984c804b255663a44b7452fba40
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-07-01 15:01:23 +0300

    removed confirm password from dto

--- src/PersonalFinancialManager.Application/DTOs/User/CreateUserDTO.cs
+++ src/PersonalFinancialManager.Application/DTOs/User/CreateUserDTO.cs
@@ -5,6 +5,4 @@ public class CreateUserDTO
     public string Email { get; set; } = null!;
 
     public string Password { get; set; } = null!;
-
-    public string ConfirmPassword { get; set; } = null!;
 }
