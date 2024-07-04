8acf13799763a6ba92b1ab7a9398ba65b3d5d65d
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-07-04 12:18:37 +0300

    Added refresh token to AppUser

--- src/PersonalFinancialManager.Data/Entities/AppUser.cs
+++ src/PersonalFinancialManager.Data/Entities/AppUser.cs
@@ -4,4 +4,7 @@ using Microsoft.AspNetCore.Identity;
 
 public class AppUser : IdentityUser<Guid>
 {
+    public string? RefreshToken { get; set; }
+
+    public DateTime? RefreshTokenExpiration { get; set; }
 }
