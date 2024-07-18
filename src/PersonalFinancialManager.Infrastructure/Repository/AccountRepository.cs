7d1556d28dbdfe65ce50dce7a54c0e97b339d812
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-07-18 14:22:37 +0300

    Added repositories for account and transaction.

---
+++ src/PersonalFinancialManager.Infrastructure/Repository/AccountRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalFinancialManager.Infrastructure.Repository;
+
+using PersonalFinancialManager.Core.Entities;
+using PersonalFinancialManager.Core.Interfaces.Repositories;
+using PersonalFinancialManager.Infrastructure.Data;
+
+public class AccountRepository(AppDbContext dbContext) : RepositoryBase<Account>(dbContext), IAccountRepository
+{
+
+}
