14b9645c13b1574c8dd6a2261a7f495bb8384028
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-07-18 11:59:55 +0300

    Added account and transaction entities.

--- src/PersonalFinancialManager.Data/Entities/AppUser.cs
+++ src/PersonalFinancialManager.Data/Entities/AppUser.cs
@@ -7,4 +7,6 @@ public class AppUser : IdentityUser<Guid>
     public string? RefreshToken { get; set; }
 
     public DateTime? RefreshTokenExpiration { get; set; }
+
+    public virtual ICollection<Account>? Accounts { get; set; }
 }
---
+++ src/PersonalFinancialManager.Data/Entities/Transaction.cs
@@ -0,0 +1,23 @@
+﻿namespace PersonalFinancialManager.Core.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using PersonalFinancialManager.Core.Enumerations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+public class Transaction
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public TransactionType Type { get; set; }
+
+    [DefaultValue(0.0)]
+    [Precision(19, 4)]
+    public decimal Amount { get; set; }
+
+    public DateTime CreationDate { get; set; }
+
+    [MaxLength(100)]
+    public string? Description { get; set; }
+}
