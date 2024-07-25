79325e44dd434928b404c538bad740098f39dd82
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-07-25 15:44:13 +0300

    Added query model and response

---
+++ src/PersonalFinancialManager.Application/ServiceModels/QueryModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PersonalFinancialManager.Application.ServiceModels;
+
+public class QueryModel
+{
+    public string? Search { get; set; }
+
+    public string? Order { get; set; }
+
+    public string? OrderBy { get; set; }
+
+    public int? Page { get; set; }
+
+    public int? ItemsPerPage { get; set; }
+}
