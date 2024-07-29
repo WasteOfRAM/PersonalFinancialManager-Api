3130f152e55de46af51e449aa8a6c1d6b9ca40c4
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-07-29 13:31:52 +0300

    Changed Account table Type column name to AccountType

---
+++ src/PersonalFinancialManager.Infrastructure/Migrations/20240729102608_ChangedTypetoAccountType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalFinancialManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedTypetoAccountType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Accounts",
+                newName: "AccountType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AccountType",
+                table: "Accounts",
+                newName: "Type");
+        }
+    }
+}
