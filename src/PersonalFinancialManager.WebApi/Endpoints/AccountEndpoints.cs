1a32d8b0b23134c366dd57d5f80acdb04718ee43
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-07-30 16:03:39 +0300

    Added account endpoint

---
+++ src/PersonalFinancialManager.WebApi/Endpoints/AccountEndpoints.cs
@@ -0,0 +1,26 @@
+﻿namespace PersonalFinancialManager.WebApi.Endpoints;
+
+using Microsoft.AspNetCore.Http.HttpResults;
+using PersonalFinancialManager.Application.DTOs.Account;
+using PersonalFinancialManager.Application.Interfaces;
+using PersonalFinancialManager.Application.ServiceModels;
+using System.Security.Claims;
+
+public static class AccountEndpoints
+{
+    public static void MapAccountEndpoints(this IEndpointRouteBuilder routes)
+    {
+        RouteGroupBuilder accountRoutes = routes.MapGroup("account");
+
+        accountRoutes.MapPost("create", async Task<Results<Ok<AccountDTO>, ValidationProblem>> (HttpContext context, CreateAccountDTO createAccountDTO, IAccountService accountService) =>
+        {
+            var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            ServiceResult<AccountDTO> serviceResult = await accountService.CreateAsync(userId!, createAccountDTO);
+
+            return serviceResult.Success ? TypedResults.Ok(serviceResult.Data) :
+                                           TypedResults.ValidationProblem(serviceResult.Errors!);
+        })
+            .MapToApiVersion(1);
+    }
+}
