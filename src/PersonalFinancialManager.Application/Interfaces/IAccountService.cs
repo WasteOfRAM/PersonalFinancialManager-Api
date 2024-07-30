432e81d9327132640750e53897c6303a92f04c4c
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-07-30 16:02:14 +0300

    Added account service

---
+++ src/PersonalFinancialManager.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonalFinancialManager.Application.Interfaces;
+
+using PersonalFinancialManager.Application.DTOs.Account;
+using PersonalFinancialManager.Application.ServiceModels;
+
+public interface IAccountService
+{
+    Task<ServiceResult<AccountDTO>> CreateAsync(string userId, CreateAccountDTO createAccountDTO);
+
+    Task<ServiceResult<AccountDTO>> GetAsync(Guid id);
+
+    Task<ServiceResult<QueryResponse<AccountDTO>>> GetAllAsync(QueryModel queryModel);
+
+    Task<ServiceResult> UpdateAsync(UpdateAccountDTO updateAccountDTO);
+
+    Task<ServiceResult> DeleteAsync(Guid id);
+}
---
+++ src/PersonalFinancialManager.Application/Services/AccountService.cs
@@ -0,0 +1,81 @@
+﻿namespace PersonalFinancialManager.Application.Services;
+
+using Microsoft.AspNetCore.Identity;
+using PersonalFinancialManager.Application.DTOs.Account;
+using PersonalFinancialManager.Application.Interfaces;
+using PersonalFinancialManager.Application.ServiceModels;
+using PersonalFinancialManager.Core.Entities;
+using PersonalFinancialManager.Core.Enumerations;
+using PersonalFinancialManager.Core.Interfaces.Repositories;
+using System;
+using System.Threading.Tasks;
+
+public class AccountService(IAccountRepository accountRepository, UserManager<AppUser> userManager) : IAccountService
+{
+    public async Task<ServiceResult<AccountDTO>> CreateAsync(string userId, CreateAccountDTO createAccountDTO)
+    {
+        var appUser = await userManager.FindByIdAsync(userId);
+
+        if (appUser == null) 
+        {
+            // TODO: return proper error
+            return new ServiceResult<AccountDTO>
+            {
+                Success = false,
+                Errors = []
+            };
+        }
+
+        Account entity = new()
+        {
+            Name = createAccountDTO.Name,
+            Currency = createAccountDTO.Currency,
+            AccountType = (AccountType)Enum.Parse(typeof(AccountType), createAccountDTO.AccountType),
+            Description = createAccountDTO.Description,
+            Total = createAccountDTO.Total,
+            CreationDate = DateTime.Now
+        };
+
+        await accountRepository.AddAsync(entity);
+        appUser.Accounts.Add(entity);
+
+        _ = await accountRepository.SaveAsync();
+
+        ServiceResult<AccountDTO> result = new ()
+        {
+            Success = true,
+            Data = new()
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Currency = entity.Currency,
+                AccountType = entity.AccountType.ToString(),
+                Description = entity.Description,
+                Total = entity.Total,
+                CreationDate = entity.CreationDate
+            }
+        };
+
+        return result;
+    }
+
+    public Task<ServiceResult> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResult<QueryResponse<AccountDTO>>> GetAllAsync(QueryModel queryModel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResult<AccountDTO>> GetAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResult> UpdateAsync(UpdateAccountDTO updateAccountDTO)
+    {
+        throw new NotImplementedException();
+    }
+}
