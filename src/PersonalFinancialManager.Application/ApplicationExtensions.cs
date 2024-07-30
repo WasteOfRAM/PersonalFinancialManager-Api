bff98fcf83cf8607d83323c75a66d698e40228c8
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-07-30 15:14:31 +0300

    Application extensions auto adding services to DI.

--- src/PersonalFinancialManager.Application/ApplicationExtensions.cs
+++ src/PersonalFinancialManager.Application/ApplicationExtensions.cs
@@ -1,11 +1,23 @@
 ﻿namespace PersonalFinancialManager.Application;
 
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 public static class ApplicationExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        var types = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsClass && !t.IsAbstract && t.ToString().EndsWith("Service")).ToList();
+
+        foreach (var type in types)
+        {
+            var @interface = type.GetInterface("I" + type.Name);
+
+            if (@interface == null)
+                continue;
+
+            services.AddScoped(@interface, type);
+        }
 
         return services;
     }
