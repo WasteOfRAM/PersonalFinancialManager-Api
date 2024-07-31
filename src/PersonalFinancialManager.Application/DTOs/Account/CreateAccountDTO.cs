d98d028319998bbb58325854f15ede8c93de2a96
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-07-31 15:45:57 +0300

    Added annotations for validations on CreateAccountDTO

--- src/PersonalFinancialManager.Application/DTOs/Account/CreateAccountDTO.cs
+++ src/PersonalFinancialManager.Application/DTOs/Account/CreateAccountDTO.cs
@@ -5,13 +5,18 @@ using System.ComponentModel.DataAnnotations;
 
 public class CreateAccountDTO
 {
+    [Required]
+    [StringLength(maximumLength: 10)]
     public string Name { get; set; } = null!;
 
+    [Required]
     public string Currency { get; set; } = null!;
 
+    [Required]
+    [EnumDataType(typeof(AccountType))]
     public string AccountType { get; set; } = null!;
 
-    public decimal Total { get; set; }
+    public decimal? Total { get; set; }
 
     public string? Description { get; set; }
 }
