6fc57f121e9a3fbb8921f6ebd2a8c0e1f99ec98e
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-08-08 16:14:38 +0300

    Changing Transactions table column from Type to TransactionType. Migration

--- src/PersonalFinancialManager.Data/Entities/Transaction.cs
+++ src/PersonalFinancialManager.Data/Entities/Transaction.cs
@@ -4,13 +4,14 @@ using Microsoft.EntityFrameworkCore;
 using PersonalFinancialManager.Core.Enumerations;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Transaction
 {
     [Key]
     public Guid Id { get; set; }
 
-    public TransactionType Type { get; set; }
+    public TransactionType TransactionType { get; set; }
 
     [DefaultValue(0.0)]
     [Precision(19, 4)]
@@ -20,4 +21,8 @@ public class Transaction
 
     [MaxLength(100)]
     public string? Description { get; set; }
+
+    public Guid AccountId { get; set; }
+    [ForeignKey(nameof(AccountId))]
+    public virtual Account? Account { get; set; }
 }
