cf21251677e5560480132782b71846beab7c0dee
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-08-08 15:23:48 +0300

    typo

--- src/PersonalFinancialManager.Infrastructure/Repository/RepositoryBase.cs
+++ src/PersonalFinancialManager.Infrastructure/Repository/RepositoryBase.cs
@@ -6,7 +6,6 @@ using PersonalFinancialManager.Application.Queries;
 using PersonalFinancialManager.Infrastructure.Data;
 using PersonalFinancialManager.Infrastructure.Extensions;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
