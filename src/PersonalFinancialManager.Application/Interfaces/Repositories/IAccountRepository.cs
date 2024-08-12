8e9cba791bcc1e6fec643004ce86f6e3855a912e
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-08-12 14:37:27 +0300

    Account repository added UpdateAccountTotal that updates the account total when a transaction is added.

--- src/PersonalFinancialManager.Application/Interfaces/Repositories/IAccountRepository.cs
+++ src/PersonalFinancialManager.Application/Interfaces/Repositories/IAccountRepository.cs
@@ -1,7 +1,9 @@
 ﻿namespace PersonalFinancialManager.Application.Interfaces.Repositories;
 
 using PersonalFinancialManager.Core.Entities;
+using PersonalFinancialManager.Core.Enumerations;
 
 public interface IAccountRepository : IRepositoryBase<Account>
 {
+    void UpdateAccountTotal(Account account, TransactionType transactionType, decimal amount);
 }
