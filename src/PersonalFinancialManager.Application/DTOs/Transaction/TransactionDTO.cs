9220ac0e72e70c603c3735e8d234eaab39c0c249
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-08-12 14:35:47 +0300

    Transaction DTO's added.

---
+++ src/PersonalFinancialManager.Application/DTOs/Transaction/TransactionDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace PersonalFinancialManager.Application.DTOs.Transaction;
+
+using PersonalFinancialManager.Core.Enumerations;
+
+public class TransactionDTO
+{
+    public Guid Id { get; set; }
+
+    public required string TransactionType { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public required string CreationDate { get; set; }
+
+    public string? Description { get; set; }
+
+    public Guid AccountId { get; set; }
+}
