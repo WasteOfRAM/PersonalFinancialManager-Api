c43b25127960daab3270260d37f83d4fe337e230
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-08-14 15:53:58 +0300

    Added endpoints for Get all, Update and Delete transactions.

--- src/PersonalFinancialManager.WebApi/Endpoints/TransactionEndpoints.cs
+++ src/PersonalFinancialManager.WebApi/Endpoints/TransactionEndpoints.cs
@@ -3,6 +3,7 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using PersonalFinancialManager.Application.DTOs.Transaction;
 using PersonalFinancialManager.Application.Interfaces.Services;
+using PersonalFinancialManager.Application.Queries;
 using PersonalFinancialManager.Application.ServiceModels;
 using PersonalFinancialManager.WebApi.Filters;
 using System.Security.Claims;
@@ -35,8 +36,48 @@ public static class TransactionEndpoints
             {
                 return TypedResults.NotFound();
             }
+            return TypedResults.Ok(serviceResult.Data);
+        })
+        .MapToApiVersion(1);
+
+        transactionRoutes.MapGet("/", async ([AsParameters] QueryModel query, ITransactionService transactionService, HttpContext context) =>
+        {
+            var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var transactions = await transactionService.GetAllAsync(query, userId!);
+
+            return TypedResults.Ok(transactions.Data);
+        })
+            .MapToApiVersion(1);
+
+        transactionRoutes.MapPut("/", async Task<Results<Ok<TransactionDTO>, NotFound>> (UpdateTransactionDTO updateTransactionDTO, ITransactionService transactionService, HttpContext context) =>
+        {
+            var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            ServiceResult<TransactionDTO> serviceResult = await transactionService.UpdateAsync(updateTransactionDTO, userId!);
+
+            if (!serviceResult.Success) 
+            { 
+                return TypedResults.NotFound(); 
+            }
 
             return TypedResults.Ok(serviceResult.Data);
+        })
+            .MapToApiVersion(1)
+            .AddEndpointFilter<ModelValidationFilter<UpdateTransactionDTO>>();
+
+        transactionRoutes.MapDelete("/{id:Guid}", async Task<Results<NoContent, NotFound>> (Guid id, ITransactionService transactionService, HttpContext context) =>
+        {
+            var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            ServiceResult serviceResult = await transactionService.DeleteAsync(id, userId!);
+
+            if (!serviceResult.Success)
+            {
+                return TypedResults.NotFound();
+            }
+
+            return TypedResults.NoContent();
         })
             .MapToApiVersion(1);
     }
