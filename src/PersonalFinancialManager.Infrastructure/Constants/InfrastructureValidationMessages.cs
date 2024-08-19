da9df5ba992fce4e8cefdbc90d3e977898127633
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-08-19 15:51:42 +0300

    Infrastructure errors constants.

---
+++ src/PersonalFinancialManager.Infrastructure/Constants/InfrastructureValidationMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonalFinancialManager.Infrastructure.Constants;
+
+public static class InfrastructureValidationMessages
+{
+    public static class ErrorMessages
+    {
+        public static class InvalidLogin
+        {
+            public const string Code = "invalid login";
+            public const string Description = "Invalid email or password.";
+        }
+    }
+}
