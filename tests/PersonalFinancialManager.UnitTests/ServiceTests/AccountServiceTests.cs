a6dd51abaf0807cedc6e172e9eebb7d4bc54c666
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-09-03 22:51:25 +0300

    AccountServiceTests added more tests.

--- tests/PersonalFinancialManager.UnitTests/ServiceTests/AccountServiceTests.cs
+++ tests/PersonalFinancialManager.UnitTests/ServiceTests/AccountServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace PersonalFinancialManager.UnitTests.ServiceTests;
 
+using NSubstitute.ReturnsExtensions;
 using PersonalFinancialManager.Application.DTOs.Account;
 using PersonalFinancialManager.Application.Interfaces.Repositories;
 using PersonalFinancialManager.Application.Services;
@@ -22,10 +23,14 @@ public class AccountServiceTests
         accountService = new AccountService(accountRepository, transactionRepository);
 
         fixture = new Fixture();
+
+        fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+            .ForEach(b => fixture.Behaviors.Remove(b));
+        fixture.Behaviors.Add(new OmitOnRecursionBehavior());
     }
 
     [Fact]
-    public async Task CreateAsync_With_Duplicate_AccountName_Returns_ResultSuccess_False_With_Error()
+    public async Task CreateAsync_With_Duplicate_AccountName_Returns_ResultSuccess_False_With_Error_With_Duplicate_AccountName_Returns_ResultSuccess_False_With_Error()
     {
         accountRepository.AnyAsync(Arg.Any<Expression<Func<Account, bool>>>()).Returns(true);
 
@@ -39,6 +44,110 @@ public class AccountServiceTests
             Assert.False(result.Success);
             Assert.NotNull(result.Errors);
             Assert.NotEmpty(result.Errors);
+        });
+    }
+
+    [Fact]
+    public async Task CreateAsync_With_Unique_Name_And_Null_Total_Returns_ResultSuccess_True_With_Data()
+    {
+        accountRepository.AnyAsync(Arg.Any<Expression<Func<Account, bool>>>()).Returns(false);
+        var createAccountDTO = fixture.Create<CreateAccountDTO>() with { Total = null };
+
+        var userId = Guid.NewGuid();
+        var result = await accountService.CreateAsync(userId.ToString(), createAccountDTO);
+
+        Assert.Multiple(() =>
+        {
+            accountRepository.ReceivedWithAnyArgs().AddAsync(Arg.Any<Account>());
+            accountRepository.Received().SaveAsync();
+            transactionRepository.DidNotReceive().AddAsync(Arg.Any<Transaction>());
+
+            Assert.True(result.Success);
+            Assert.Null(result.Errors);
+            Assert.NotNull(result.Data);
+        });
+    }
+
+    [Fact]
+    public async Task CreateAsync_With_Unique_Name_And_Zero_Total_Returns_ResultSuccess_True_With_Data()
+    {
+        accountRepository.AnyAsync(Arg.Any<Expression<Func<Account, bool>>>()).Returns(false);
+        var createAccountDTO = fixture.Build<CreateAccountDTO>()
+            .With(a => a.Total, 0.0m)
+            .Create();
+
+        var userId = Guid.NewGuid();
+        var result = await accountService.CreateAsync(userId.ToString(), createAccountDTO);
+
+        Assert.Multiple(() =>
+        {
+            accountRepository.Received().AddAsync(Arg.Any<Account>());
+            accountRepository.Received().SaveAsync();
+            transactionRepository.DidNotReceive().AddAsync(Arg.Any<Transaction>());
+
+            Assert.True(result.Success);
+            Assert.Null(result.Errors);
+            Assert.NotNull(result.Data);
+        });
+    }
+
+    [Fact]
+    public async Task CreateAsync_With_Unique_Name_And_GreaterThan_Zero_Total_Returns_ResultSuccess_True_With_Data_And_Calls_TransactionRepository_AddAsync()
+    {
+        accountRepository.AnyAsync(Arg.Any<Expression<Func<Account, bool>>>()).Returns(false);
+        var createAccountDTO = fixture.Build<CreateAccountDTO>()
+            .With(a => a.Total, 79797.111m)
+            .Create();
+
+        var userId = Guid.NewGuid();
+        var result = await accountService.CreateAsync(userId.ToString(), createAccountDTO);
+
+        Assert.Multiple(() =>
+        {
+            accountRepository.Received().AddAsync(Arg.Any<Account>());
+            accountRepository.Received().SaveAsync();
+            transactionRepository.Received().AddAsync(Arg.Any<Transaction>());
+
+            Assert.True(result.Success);
+            Assert.Null(result.Errors);
+            Assert.NotNull(result.Data);
+        });
+    }
+
+    [Fact]
+    public async Task DeleteAsync_With_Non_Existing_Id_Or_Not_Owning_User_Returns_ResultSuccess_False()
+    {
+        accountRepository.GetAsync(Arg.Any<Expression<Func<Account, bool>>>()).ReturnsNull();
+
+        var id = Guid.NewGuid();
+        var userId = Guid.NewGuid().ToString();
+
+        var result = await accountService.DeleteAsync(id, userId);
+
+        Assert.Multiple(() =>
+        {
+            Assert.False(result.Success);
+
+            accountRepository.DidNotReceive().Delete(Arg.Any<Account>());
+            accountRepository.DidNotReceive().SaveAsync();
+        });
+    }
+
+    [Fact]
+    public async Task DeleteAsync_With_Existing_Id_And_Owning_User_Returns_ResultSuccess_True()
+    {
+        var accountEntity = fixture.Create<Account>();
+
+        accountRepository.GetAsync(Arg.Any<Expression<Func<Account, bool>>>()).Returns(accountEntity);
+
+        var result = await accountService.DeleteAsync(accountEntity.Id, accountEntity.AppUserId.ToString());
+
+        Assert.Multiple(() =>
+        {
+            Assert.True(result.Success);
+
+            accountRepository.Received().Delete(accountEntity);
+            accountRepository.Received().SaveAsync();
         });
     }
 }
