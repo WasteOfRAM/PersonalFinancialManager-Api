192a2a1008563a101d729bf27a8c2d4312d53cc1
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-09-09 13:45:38 +0300

    TransactionService tests setup.

---
+++ tests/PersonalFinancialManager.UnitTests/ServiceTests/TransactionServiceTests.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonalFinancialManager.UnitTests.ServiceTests;
+
+using PersonalFinancialManager.Application.Interfaces.Repositories;
+using PersonalFinancialManager.Application.Services;
+
+public class TransactionServiceTests
+{
+    private readonly IAccountRepository accountRepository;
+    private readonly ITransactionRepository transactionRepository;
+    private readonly TransactionService transactionService;
+
+    private readonly Fixture fixture;
+
+    public TransactionServiceTests()
+    {
+        transactionRepository = Substitute.For<ITransactionRepository>();
+        accountRepository = Substitute.For<IAccountRepository>();
+
+        transactionService = new TransactionService(transactionRepository, accountRepository);
+
+        fixture = new Fixture();
+
+        fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+            .ForEach(b => fixture.Behaviors.Remove(b));
+        fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+    }
+}
