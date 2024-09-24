1a7b77d34740c3e58bb38a821eeb5fb85dd3c07f
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-09-24 11:49:39 +0300

    Some test constants.

---
+++ tests/PersonalFinancialManager.IntegrationTests/Constants/EndpointsV1.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalFinancialManager.IntegrationTests.Constants;
+
+public static class EndpointsV1
+{
+    public static class UserEndpoints
+    {
+        public const string Register = "/api/v1/user/register";
+        public const string Login = "/api/v1/user/login";
+        public const string Refresh = "/api/v1/user/refresh";
+    }
+}
---
+++ tests/PersonalFinancialManager.IntegrationTests/Constants/Passwords.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalFinancialManager.IntegrationTests.Constants;
+
+public static class Passwords
+{
+    public const string ApplicationValidPassword = "Te123456789$";
+    public const string ApplicationInvalidPasswordOne = "Te1234$";
+    public const string ApplicationInvalidPasswordTwo = "Te123456789";
+    public const string ApplicationInvalidPasswordThree = "te123456789$";
+}
