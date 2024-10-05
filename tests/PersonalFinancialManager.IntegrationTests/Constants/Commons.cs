cf21b485befa1b5c03e9436e91575a95cc129565
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-10-05 14:27:35 +0300

    User enpointsv1 tests refactoring.

---
+++ tests/PersonalFinancialManager.IntegrationTests/Constants/Commons.cs
@@ -0,0 +1,19 @@
+﻿namespace PersonalFinancialManager.IntegrationTests.Constants;
+
+public static class Commons
+{
+    public static class UserEmails
+    {
+        public const string TestUserEmail = "user@test.com";
+    }
+
+    public static class Passwords
+    {
+        public const string ApplicationValidPassword = "Te123456789$";
+        public const string ApplicationValidPasswordTwo = "aG987654321*";
+
+        public const string ApplicationInvalidPasswordOne = "Te1234$";
+        public const string ApplicationInvalidPasswordTwo = "Te123456789";
+        public const string ApplicationInvalidPasswordThree = "te123456789$";
+    }
+}
