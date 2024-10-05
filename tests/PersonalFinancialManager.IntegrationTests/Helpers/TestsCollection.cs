5100846b99614747fe9ae3b09d0791df8d3c51c2
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-10-05 14:26:58 +0300

    Added Test collection to combine fixtures.

---
+++ tests/PersonalFinancialManager.IntegrationTests/Helpers/TestsCollection.cs
@@ -0,0 +1,8 @@
+﻿[assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = false)]
+
+namespace PersonalFinancialManager.IntegrationTests.Helpers;
+
+[CollectionDefinition("Tests collection", DisableParallelization = false)]
+public class TestsCollection : ICollectionFixture<TestsFixture>
+{
+}
