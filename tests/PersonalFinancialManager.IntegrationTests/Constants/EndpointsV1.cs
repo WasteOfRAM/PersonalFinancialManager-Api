e8d16fcdbc5897b3938126f7868813aac8f6f348
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-10-06 15:38:51 +0300

    Account endpoints added to constants

--- tests/PersonalFinancialManager.IntegrationTests/Constants/EndpointsV1.cs
+++ tests/PersonalFinancialManager.IntegrationTests/Constants/EndpointsV1.cs
@@ -8,4 +8,9 @@ public static class EndpointsV1
         public const string Login = "/api/v1/user/login";
         public const string Refresh = "/api/v1/user/refresh";
     }
+
+    public static class AccountEndpoints
+    {
+        public const string Account = "/api/v1/account";
+    }
 }
