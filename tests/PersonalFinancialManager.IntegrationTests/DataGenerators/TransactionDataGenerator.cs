3232828f6543cc2f75657b47e71f942bad1df79d
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-10-07 12:04:44 +0300

    Data generator for transactions added.

---
+++ tests/PersonalFinancialManager.IntegrationTests/DataGenerators/TransactionDataGenerator.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonalFinancialManager.IntegrationTests.DataGenerators;
+
+using Bogus;
+using PersonalFinancialManager.Core.Entities;
+using PersonalFinancialManager.Core.Enumerations;
+using System.Globalization;
+
+using static PersonalFinancialManager.Application.Constants.ApplicationCommonConstants;
+using static PersonalFinancialManager.Core.Constants.ValidationConstants;
+
+public class TransactionDataGenerator(Guid accountId)
+{
+    private static readonly decimal minValue = decimal.Parse(DecimalRangeMinimumValue, CultureInfo.InvariantCulture);
+    private static readonly decimal maxValue = decimal.Parse(DecimalRangeMaximumValue, CultureInfo.InvariantCulture);
+
+    private readonly Faker<Transaction> transactionEntities = new Faker<Transaction>()
+        .RuleFor(p => p.TransactionType, f => f.Random.Enum<TransactionType>())
+        .RuleFor(p => p.Amount, f => f.Finance.Amount(minValue, maxValue, DecimalPrecisionConstant.Scale))
+        .RuleFor(p => p.CreationDate, f => f.Date.Past(1, DateTime.UtcNow))
+        .RuleFor(p => p.Description, f => f.Random.Words().OrNull(f, 0.3f))
+        .RuleFor(p => p.AccountId, accountId);
+
+    public List<Transaction> GenerateTransactionEntities(int itemCount = 1)
+    {
+        return transactionEntities.Generate(itemCount);
+    }
+}
