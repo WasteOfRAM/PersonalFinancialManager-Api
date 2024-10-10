9a62aa3732ca70aa352e03c8e90091cde7f1a001
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-10-10 15:21:04 +0300

    Added GenerateUpdateTransactionDTO

--- tests/PersonalFinancialManager.IntegrationTests/DataGenerators/TransactionDataGenerator.cs
+++ tests/PersonalFinancialManager.IntegrationTests/DataGenerators/TransactionDataGenerator.cs
@@ -1,6 +1,7 @@
 ﻿namespace PersonalFinancialManager.IntegrationTests.DataGenerators;
 
 using Bogus;
+using PersonalFinancialManager.Application.DTOs.Transaction;
 using PersonalFinancialManager.Core.Entities;
 using PersonalFinancialManager.Core.Enumerations;
 using System.Globalization;
@@ -20,8 +21,33 @@ public class TransactionDataGenerator(Guid accountId)
         .RuleFor(p => p.Description, f => f.Random.Words().OrNull(f, 0.3f))
         .RuleFor(p => p.AccountId, accountId);
 
+    private readonly Faker<CreateTransactionDTO> createTransactionDTO = new Faker<CreateTransactionDTO>()
+        .CustomInstantiator(f => new CreateTransactionDTO(
+            accountId.ToString(),
+            f.Random.Enum<TransactionType>().ToString(),
+            f.Finance.Amount(minValue, maxValue, DecimalPrecisionConstant.Scale),
+            f.Random.Words().OrNull(f, 0.3f)));
+
+    private readonly Faker<UpdateTransactionDTO> updateTransactionDTO = new Faker<UpdateTransactionDTO>()
+        .CustomInstantiator(f => new UpdateTransactionDTO(
+            Guid.Empty.ToString(),
+            accountId.ToString(),
+            f.Random.Enum<TransactionType>().ToString(),
+            f.Finance.Amount(minValue, maxValue, DecimalPrecisionConstant.Scale),
+            f.Random.Words().OrNull(f, 0.3f)));
+
     public List<Transaction> GenerateTransactionEntities(int itemCount = 1)
     {
         return transactionEntities.Generate(itemCount);
+    }
+
+    public CreateTransactionDTO GenerateCreateTransactionDTO()
+    {
+        return createTransactionDTO.Generate();
+    }
+
+    public UpdateTransactionDTO GenerateUpdateTransactionDTO(string transactionId) 
+    {
+        return updateTransactionDTO.Generate() with { Id = transactionId};
     }
 }
