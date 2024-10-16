fbe972105f1a2ba1c151f5b09d925e3f45d03f1e
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-10-16 15:34:32 +0300

    Transaction service validations.
    Added validations for Account Total update for min and max range on transaction update and transaction delete.

--- src/PersonalFinancialManager.Application/Constants/ApplicationValidationMessages.cs
+++ src/PersonalFinancialManager.Application/Constants/ApplicationValidationMessages.cs
@@ -15,5 +15,29 @@ public static class ApplicationValidationMessages
             public const string Code = "AccountId";
             public const string Description = "Account with given Id does not exist.";
         }
+
+        public static class AccountTotalMinValue
+        {
+            public const string Code = "Insufficient funds";
+            public const string Description = "Withdraw amount is higher than the current account total.";
+        }
+
+        public static class AccountTotalMaxValue
+        {
+            public const string Code = "Account total limit";
+            public const string Description = "Deposit amount will exceed the account total limit. ({0})";
+        }
+
+        public static class ForbiddenTransactionDeletion
+        {
+            public const string Code = "Forbidden deletion";
+            public const string Description = "Only latest transaction can be deleted. Try updating the transaction or create a new one with needed corrections.";
+        }
+
+        public static class ForbiddenTransactionEdit
+        {
+            public const string Code = "Forbidden edit";
+            public const string Description = "Only latest transaction can be edited. Try creating a new one with needed corrections.";
+        }
     }
 }
